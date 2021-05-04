0efc78ade916633f5a4356dc7b7d5a4db11b2360
Simona Maggio <maggio.simona@gmail.com>
2021-05-04 13:13:12 +0200

    minor rename

--- python-lib/dku_stress_test_center/stress_test_center.py
+++ python-lib/dku_stress_test_center/stress_test_center.py
@@ -77,7 +77,7 @@ class DkuModelHandler(object):
         except Exception as e:
             from future.utils import raise_
             if "ordinal not in range(128)" in safe_str(e):
-                raise_(Exception, "Model Error Analysis requires models built with python3. This one is on python2.",
+                raise_(Exception, "Stress Test Center requires models built with python3. This one is on python2.",
                        sys.exc_info()[2])
             else:
                 raise_(Exception, "Fail to load saved model: {}".format(e), sys.exc_info()[2])
