803b0755deb398a56fc8da8d95765033c62dee4c
admin <admin>
2021-05-06 21:37:01 +0200

    update backend

--- custom-recipes/stress-test-text-model/recipe.py
+++ custom-recipes/stress-test-text-model/recipe.py
@@ -1,51 +1,104 @@
 import dataiku
 from dataiku.customrecipe import *
-from load_parameters import load_samples_column,load_severity,load_attacks_type
-from drift_dac.covariate_shift import ReplaceWord,Typos,WordDeletion
-from dku_stress_test_center.stress_test_center import StressTestConfiguration, StressTestGenerator
-from dku_stress_test_center.stress_test_center import StressTestGenerator, StressTestConfiguration, build_stress_metric, get_critical_samples
+from dku_config_parameters.load_parameters import DkuConfigLoadingStressTestCenter
+from drift_dac.covariate_shift import ReplaceWord, Typos, WordDeletion
+from dku_stress_test_center.stress_test_center import (
+    StressTestConfiguration,
+    StressTestGenerator,
+    build_stress_metric,
+    get_critical_samples,
+)
+from dku_stress_test_center.model_accessor import ModelAccessor
 from dku_stress_test_center.utils import DkuStressTestCenterConstants
+from model_metadata import get_model_handler
+import numpy as np
 
-model_input_name = get_input_names_for_role("model")
-model_input = [dataiku.Model(name) for name in model_input_name][0]
+# load recipe datasets and parameters
+dku_config = DkuConfigLoadingStressTestCenter()
+settings = dku_config.load_settings()
+
+# get model target
+model_handler = get_model_handler(model=settings.input_model, version_id=None)
+model_accessor = ModelAccessor(model_handler)
 target = model_accessor.get_target_variable()
 
-samples_input_name = get_input_names_for_role("input_dataset")
-samples_input = [dataiku.Dataset(name) for name in dataset_input_name][0]
-samples_df = samples_input.get_dataframe()
-
-samples_column = load_samples_column()
-severity = load_severity()
-
+# instanciate the class implementing the perturbations to apply
 config_list = []
-typos_shift,word_swap_shift,word_deletion_shift = load_attacks_type()
-if word_swap_shift:
-    config_list.append(StressTestConfiguration(ReplaceWord(samples_fraction=severity["word_swap"])))
-if typos_shift:
-    config_list.append(StressTestConfiguration(Typos(samples_fraction=severity["typos"])))
-if word_deletion_shift:
-    config_list.append(StressTestConfiguration(WordDeletion(samples_fraction=severity["word_deletion"])))
+if settings.word_swap:
+    config_list.append(
+        StressTestConfiguration(
+            ReplaceWord(samples_fraction=settings.word_swap_severity)
+        )
+    )
+if settings.typos:
+    config_list.append(
+        StressTestConfiguration(Typos(samples_fraction=settings.typos_severity))
+    )
+if settings.word_deletion:
+    config_list.append(
+        StressTestConfiguration(
+            WordDeletion(samples_fraction=settings.word_deletion_severity)
+        )
+    )
 
 
-stressor = StressTestGenerator(config_list=config_list,
-                               selected_features = [samples_column],
-                               is_categorical = [False],
-                               is_text=[True])
+stressor = StressTestGenerator(
+    config_list=config_list,
+    selected_features=[settings.samples_column],
+    is_categorical=np.array([False]),
+    is_text=np.array([True]),
+)
 
-perturbed_df = stressor.fit_transform(test_df, target)
+# apply perturbation to the input dataset
+samples_df = settings.input_dataset.get_dataframe()
+perturbed_df = stressor.fit_transform(samples_df, target)
 perturbed_df_with_prediction = model_accessor.predict(perturbed_df)
 
-# Compute the performance drop metrics
-metrics_df = build_stress_metric(y_true=perturbed_df[target],
-                                 y_pred=perturbed_df_with_prediction['prediction'],
-                                 stress_test_indicator=perturbed_df[DkuStressTestCenterConstants.STRESS_TEST_TYPE],
-                                 pos_label='positive') # TODO this is hardcoding
+# compute metrics
+metrics_df = build_stress_metric(
+    y_true=perturbed_df[target],
+    y_pred=perturbed_df_with_prediction["prediction"],
+    stress_test_indicator=perturbed_df[DkuStressTestCenterConstants.STRESS_TEST_TYPE],
+    pos_label="positive",
+)  # TODO this is hardcoding
 
-metrics_output = get_output_names_for_role("metrics_dataset")
-metrics_output_dataset = [dataiku.Dataset(name) for name in metrics_output][0]
+# compute critical samples
+name_mapping = {
+    "REPLACE_WORD": "Adversarial attack",
+    "TYPOS": "Missing values",
+    "WORD_DELETION": "Word deletion",
+}
 
-critical_samples = get_output_names_for_role("critical_samples_dataset")
-critical_samples_dataset = [dataiku.Dataset(name) for name in critical_samples][0]
+metrics_list = []
+for index, row in metrics_df.iterrows():
+    dct = {}
+    dct["attack_type"] = name_mapping.get(row["_dku_stress_test_type"])
+    dct["accuracy_drop"] = 100 * round(row["accuracy_drop"], 3)
+    dct["robustness"] = 100 * round(row["robustness"], 3)
+    metrics_list.append(dct)
 
-metrics_output_dataset.write_with_schema(metrics_df)
-critical_samples_dataset.write_with_schema(dataset_input)
+y_true = perturbed_df[target]
+y_true_class_confidence = perturbed_df_with_prediction[["proba_0", "proba_1"]].values
+y_true_idx = np.array([[True, False] if y == "1" else [False, True] for y in y_true])
+y_true_class_confidence = y_true_class_confidence[y_true_idx]
+
+critical_samples_id_df = get_critical_samples(
+    y_true_class_confidence=y_true_class_confidence,
+    stress_test_indicator=perturbed_df[DkuStressTestCenterConstants.STRESS_TEST_TYPE],
+    row_indicator=perturbed_df[DkuStressTestCenterConstants.DKU_ROW_ID],
+)
+
+# TODO hot fix that should be done inside get_critical_samples
+critical_samples_id_df.reset_index(level=0, inplace=True)
+
+clean_df_with_id = perturbed_df.loc[
+    perturbed_df[DkuStressTestCenterConstants.STRESS_TEST_TYPE]
+    == DkuStressTestCenterConstants.CLEAN
+].drop(DkuStressTestCenterConstants.STRESS_TEST_TYPE, axis=1)
+critical_samples_df = critical_samples_id_df.merge(
+    clean_df_with_id, on=DkuStressTestCenterConstants.DKU_ROW_ID, how="left"
+).drop(DkuStressTestCenterConstants.DKU_ROW_ID, axis=1)
+
+# write output datasets
+settings.metrics_output_dataset.write_with_schema(metrics_df)
+settings.critical_samples_dataset.write_with_schema(critical_samples_df)
---
+++ python-lib/dku_config_parameters/load_parameters.py
@@ -0,0 +1,128 @@
+import dataiku
+from dataiku.customrecipe import (
+    get_recipe_config,
+    get_input_names_for_role,
+    get_output_names_for_role,
+)
+from dku_config_parameters.dku_config import DkuConfig
+
+
+class DkuConfigLoading:
+    def __init__(self):
+        self.config = get_recipe_config()
+        self.dku_config = DkuConfig()
+
+
+class DkuConfigLoadingStressTestCenter(DkuConfigLoading):
+    """Configuration for Ontology Tagging Plugin"""
+
+    ATTACKS_PARAMETERS = [
+        "typos",
+        "word_swap",
+        "word_deletion",
+    ]
+    ATTACKS_SEVERITY_PARAMETERS = [
+        "typos_severity",
+        "word_swap_severity",
+        "word_deletion_severity",
+    ]
+
+    def __init__(self):
+        """Instanciate class with DkuConfigLoading and add input datasets to dku_config"""
+
+        super().__init__()
+        input_dataset = get_input_names_for_role("input_dataset")[0]
+        self.dku_config.add_param(
+            name="input_dataset", value=dataiku.Dataset(input_dataset), required=True
+        )
+        self.input_dataset_columns = [
+            p["name"] for p in self.dku_config.input_dataset.read_schema()
+        ]
+        input_model = get_input_names_for_role("model")[0]
+        self.dku_config.add_param(
+            name="input_model", value=dataiku.Model(input_model), required=True
+        )
+
+    def _content_error_message(self, error, column):
+        """Get corresponding error message if any"""
+
+        if error == "missing":
+            return "Missing input column."
+
+        if error == "invalid":
+            return "Invalid input column : {}.\n".format(column)
+
+    def _add_samples_column(self):
+        """Load samples column from Test Dataset"""
+
+        text_column = self.config.get("samples_column")
+        input_columns = self.input_dataset_columns
+        self.dku_config.add_param(
+            name="samples_column",
+            value=text_column,
+            required=True,
+            checks=self._get_column_checks(text_column, input_columns),
+        )
+
+    def _get_column_checks(self, column, input_columns):
+        """Check for mandatory columns parameters"""
+
+        return [
+            {
+                "type": "exists",
+                "err_msg": self._content_error_message("missing", None),
+            },
+            {
+                "type": "custom",
+                "cond": column in input_columns or column == None,
+                "err_msg": self._content_error_message("invalid", column),
+            },
+        ]
+
+    def _add_attacks(self):
+        """Load matching parameters"""
+
+        for parameter in self.ATTACKS_PARAMETERS:
+            self.dku_config.add_param(
+                name=parameter, value=self.config[parameter], required=True
+            )
+
+    def _add_severity_(self, attack_severity):
+        for parameter in self.ATTACKS_SEVERITY_PARAMETERS:
+            self.dku_config.add_param(
+                name=parameter,
+                value=self.config.get(parameter),
+                checks=[
+                    {
+                        "type": "between",
+                        "op": (0, 1),
+                        "err_msg": "The fraction of the samples to corrupt should be between 0 and 1.",
+                    }
+                ],
+            )
+
+    def _add_output_metrics_dataset(self):
+        metrics_output = get_output_names_for_role("metrics_dataset")[0]
+        self.dku_config.add_param(
+            name="metrics_dataset",
+            value=dataiku.Dataset(metrics_output),
+            required=True,
+        )
+
+    def _add_output_critical_samples_dataset(self):
+        critical_samples_output = get_output_names_for_role("critical_samples_dataset")[
+            0
+        ]
+        self.dku_config.add_param(
+            name="critical_samples_dataset",
+            value=dataiku.Dataset(critical_samples_output),
+            required=True,
+        )
+
+    def load_settings():
+        self._add_samples_column()
+        self._add_attacks()
+        self._add_severity()
+        self._add_output_metrics_dataset()
+        self._add_output_critical_samples_dataset()
+        return self.dku_config()
