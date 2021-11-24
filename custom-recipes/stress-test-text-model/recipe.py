c0f03774f66570c8867d0c8d3cf28f551db6690d
Agathe Guillemot <agathe.guillemot@dataiku.com>
2021-11-24 16:44:20 +0100

    Read and write dataset by chunks

--- custom-recipes/stress-test-text-model/recipe.py
+++ custom-recipes/stress-test-text-model/recipe.py
@@ -3,15 +3,16 @@ from dataiku.customrecipe import get_input_names_for_role, get_output_names_for_
 from dku_stress_test_center.stress_test_center import FeaturePerturbationTest
 from dku_stress_test_center.utils import DkuStressTestCenterConstants
 
-input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
-input_dataframe = input_dataset.get_dataframe()
-
-corrupted_dataset = dataiku.Dataset(get_output_names_for_role("corrupted_dataset")[0])
 recipe_config = get_recipe_config()
-
 test_class, _ = DkuStressTestCenterConstants.TESTS[recipe_config["stress_test"]]
 feature_perturbation = test_class(samples_fraction=recipe_config["samples_fraction"])
 stress_test = FeaturePerturbationTest(feature_perturbation, recipe_config["selected_features"])
-corrupted_dataframe = stress_test.perturb_df(input_dataframe)
 
-corrupted_dataset.write_with_schema(corrupted_dataframe) # TODO: by batches?
+input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
+corrupted_dataset = dataiku.Dataset(get_output_names_for_role("corrupted_dataset")[0])
+corrupted_dataset.write_schema(input_dataset.read_schema())
+
+with corrupted_dataset.get_writer() as writer:
+    for df_chunk in input_dataset.iter_dataframes():
+        corrupted_dataframe = stress_test.perturb_df(df_chunk)
+        writer.write_dataframe(corrupted_dataframe) # TODO: by batches?
