2278842c0d86dcc1a8f64564203b3f04fbb063dd
Agathe Guillemot <agathe.guillemot@dataiku.com>
2022-01-05 12:18:04 +0100

    Allow user to select the perf metric

--- webapps/stress-test-center-view/app.js
+++ webapps/stress-test-center-view/app.js
@@ -6,21 +6,11 @@ const versionId = webAppConfig['versionId'];
     'use strict';
     app.service("CorruptionUtils", function(MetricNames) {
         function metrics(metric, isRegression) {
-            const [shortName, longName] = [
-                MetricNames[metric.actual].shortName || metric.actual,
-                MetricNames[metric.actual].longName
-            ];
-
-            let metricUsedDesc;
-            if (metric.initial === "CUSTOM") {
-                metricUsedDesc = "the default metric for "+
-                `${isRegression ? "regression tasks (R2 score)" : "classification tasks (ROC AUC)"}.`;
-            } else {
-                metricUsedDesc = `the model's hyperparameter optimization metric (here, ${longName}).`;
-            }
+            const shortName = MetricNames.shortName(metric);
+            const longName = MetricNames.longName(metric).toLowerCase();
 
             const perfVarDesc = "Performance variation is the difference in the model's performance " +
-            `between the altered and unaltered dataset. Performance is assessed via ${metricUsedDesc}`;
+            `between the altered and unaltered dataset. Performance is assessed via the ${longName}.`;
 
             const resilienceDescClassif = "Corruption resilience is the ratio of rows where " +
                 "the prediction is not altered after the corruption.";
@@ -41,7 +31,7 @@ const versionId = webAppConfig['versionId'];
                         contextual: true
                     },
                     {
-                        name: "performance_variation",
+                        name: "perf_var",
                         displayName: `${shortName} variation`,
                         description: perfVarDesc
                     },
@@ -63,7 +53,7 @@ const versionId = webAppConfig['versionId'];
                         contextual: true
                     },
                     {
-                        name: "performance_variation",
+                        name: "perf_var",
                         displayName: `${shortName} variation`,
                         description: perfVarDesc
                     },
@@ -92,67 +82,86 @@ const versionId = webAppConfig['versionId'];
         };
     });
 
-    app.constant('MetricNames', {
-        F1: {
-            longName: "F1 score",
-        },
-        ACCURACY: {
-            longName: "accuracy",
-            shortName: "Accuracy"
-        },
-        PRECISION: {
-            longName: "precision",
-            shortName: "Precision"
-        },
-        RECALL: {
-            longName: "recall",
-            shortName: "Recall"
-        },
-        COST_MATRIX: {
-            longName: "cost matrix gain",
-            shortName: "Cost matrix gain"
-        },
-        ROC_AUC: {
-            longName: "AUC",
-            shortName: "AUC"
-        },
-        LOG_LOSS: {
-            longName: "log loss",
-            shortName: "Log loss"
-        },
-        CUMULATIVE_LIFT: {
-            longName: "cumulative lift",
-            shortName: "Lift"
-        },
-        EVS: {
-            longName: "explained variance score",
-        },
-        MAPE: {
-            longName: "mean absolute percentage error"
-        },
-        MAE: {
-            longName: "mean absolute error"
-        },
-        MSE: {
-            longName: "mean squared error"
-        },
-        RMSE: {
-            longName: "root mean square error"
-        },
-        RMSLE: {
-            longName: "root mean square logarithmic error"
-        },
-        R2: {
-            longName: "R2 score"
+    app.service('MetricNames', function() { 
+        const metrics = {
+            F1: {
+                longName: "F1 score",
+                predType: ["BINARY_CLASSIFICATION", "MULTICLASS"]
+            },
+            ACCURACY: {
+                shortName: "Accuracy",
+                predType: ["BINARY_CLASSIFICATION", "MULTICLASS"]
+            },
+            PRECISION: {
+                shortName: "Precision",
+                predType: ["BINARY_CLASSIFICATION", "MULTICLASS"]
+            },
+            RECALL: {
+                shortName: "Recall",
+                predType: ["BINARY_CLASSIFICATION", "MULTICLASS"]
+            },
+            COST_MATRIX: {
+                shortName: "Cost matrix gain",
+                predType: ["BINARY_CLASSIFICATION"]
+            },
+            ROC_AUC: {
+                shortName: "AUC",
+                predType: ["BINARY_CLASSIFICATION", "MULTICLASS"]
+            },
+            LOG_LOSS: {
+                shortName: "Log loss",
+                predType: ["BINARY_CLASSIFICATION", "MULTICLASS"]
+            },
+            CUMULATIVE_LIFT: {
+                longName: "Cumulative lift",
+                shortName: "Lift",
+                predType: ["BINARY_CLASSIFICATION"]
+            },
+            EVS: {
+                longName: "Explained variance score",
+                predType: ["REGRESSION"]
+            },
+            MAPE: {
+                longName: "Mean absolute percentage error",
+                predType: ["REGRESSION"]
+            },
+            MAE: {
+                longName: "Mean absolute error",
+                predType: ["REGRESSION"]
+            },
+            MSE: {
+                longName: "Mean squared error",
+                predType: ["REGRESSION"]
+            },
+            RMSE: {
+                longName: "Root mean square error",
+                predType: ["REGRESSION"]
+            },
+            RMSLE: {
+                longName: "Root mean square logarithmic error",
+                predType: ["REGRESSION"]
+            },
+            R2: {
+                longName: "R2 score",
+                predType: ["REGRESSION"]
+            }
+        }
+        return {
+            availableMetrics: function(predType) {
+                return Object.keys(metrics).filter(_ => metrics[_].predType.includes(predType));
+            },
+            longName: metric => metrics[metric].longName || metrics[metric].shortName,
+            shortName: metric => metrics[metric].shortName || metric
         }
     });
-
-    app.controller('VizController', function($scope, $http, ModalService, CorruptionUtils, $filter) {
+        
+    app.controller('VizController', function($scope, $http, ModalService, CorruptionUtils, MetricNames, $filter) {
         $scope.modal = {};
         $scope.removeModal = ModalService.remove($scope.modal);
         $scope.createModal = ModalService.create($scope.modal);
 
         $scope.CORRUPTION_TYPES = CorruptionUtils.types;
+        $scope.userFriendlyMetricName = MetricNames.longName
 
         $scope.loading = {};
         $scope.forms = {};
@@ -196,7 +205,7 @@ const versionId = webAppConfig['versionId'];
         }
 
         $scope.checkTestConfig = function() {
-            if (!$scope.forms.SAMPLES || $scope.forms.SAMPLES.$invalid) return { canRun: false };
+            if (!$scope.forms.GENERAL || $scope.forms.GENERAL.$invalid) return { canRun: false };
             const testEntries = Object.entries($scope.tests.perturbations);
             const validActivatedTests = testEntries.filter(function(entry) {
                 const [testName, testSettings] = entry;
@@ -226,26 +235,27 @@ const versionId = webAppConfig['versionId'];
                 }
                 return fullParams;
             }, {});
+            $scope.CORRUPTION_METRICS = CorruptionUtils.metrics(
+                requestParams.perfMetric,  $scope.modelInfo.predType === 'REGRESSION'
+            );
 
             $scope.loading.results = true;
-            $http.post(
-                getWebAppBackendUrl("stress-tests-config"), requestParams).then(function() {
-                $http.get(getWebAppBackendUrl("compute"))
-                    .then(function(response) {
-                        angular.forEach(response.data, function(result) {
-                            if (result.critical_samples) {
-                                result.critical_samples.predList = result.critical_samples.predList.map(function(predList) {
-                                    predList = Object.entries(predList).map(function(pred) {
-                                        const [testName, result] = pred;
-                                        return `${CorruptionUtils.TEST_NAMES[testName]}: ${$filter("toFixedIfNeeded")(result, 3)}`;
-                                    });
-                                    predList.unshift($scope.modelInfo.isRegression ? "Predicted values" : "True class probas");
-                                    return predList;
+            $http.post(getWebAppBackendUrl("stress-tests-config"), requestParams).then(function() {
+                $http.get(getWebAppBackendUrl("compute")).then(function(response) {
+                    angular.forEach(response.data, function(result) {
+                        if (result.critical_samples) {
+                            result.critical_samples.predList = result.critical_samples.predList.map(function(predList) {
+                                predList = Object.entries(predList).map(function(pred) {
+                                    const [testName, result] = pred;
+                                    return `${CorruptionUtils.TEST_NAMES[testName]}: ${$filter("toFixedIfNeeded")(result, 3)}`;
                                 });
-                            }
-                        });
-                        $scope.results = response.data;
-                        $scope.loading.results = false;
+                                predList.unshift($scope.modelInfo.predType === 'REGRESSION' ? "Predicted values" : "True class probas");
+                                return predList;
+                            });
+                        }
+                    });
+                    $scope.results = response.data;
+                    $scope.loading.results = false;
                 }, function(e) {
                     $scope.loading.results = false;
                     $scope.createModal.error(e.data);
@@ -262,9 +272,10 @@ const versionId = webAppConfig['versionId'];
             .then(function(response) {
                 $scope.loading.modelInfo = false;
                 $scope.modelInfo.targetClasses = response.data["target_classes"];
-                $scope.modelInfo.isRegression = !$scope.modelInfo.targetClasses.length;
+                $scope.modelInfo.predType = response.data["pred_type"];
 
-                $scope.CORRUPTION_METRICS = CorruptionUtils.metrics(response.data["metric"],  $scope.modelInfo.isRegression);
+                $scope.tests.perfMetric = response.data["metric"];
+                $scope.METRIC_NAMES = MetricNames.availableMetrics($scope.modelInfo.predType);
 
                 features = response.data["features"];
 
@@ -281,7 +292,7 @@ const versionId = webAppConfig['versionId'];
                         }
                     }
                     if (testConfig.needsTargetClasses) {
-                        if ($scope.modelInfo.isRegression) {
+                        if ($scope.modelInfo.predType === 'REGRESSION') {
                             delete $scope.tests.perturbations[testName];
                         }
                     }
